d52b83452950d3ccf01e921f8aa4f249942013af
Replit user <>
2022-09-10 13:06:27 +0200

    adding icons

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch.png" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
